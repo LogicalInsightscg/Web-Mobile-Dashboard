e9172ecd3c9b6dca1205a9b9f0435e7d67a89dc1
Amit Kumar Sah <amitkumars954@gmail.com>
2017-02-08 14:53:31 +0530

    File of updating the database using kendo grid

--- Goals_data.php
+++ Goals_data.php
@@ -27,36 +27,44 @@ $arr= array();
 
       echo json_encode($arr);
     }
-/*
+
 // handle a POST  
     if ($verb == "POST") {
 
- // DISCLAIMER: It is better to use PHP prepared statements to communicate with the database. 
-        //             this provides better protection against SQL injection.
-        //             [http://php.net/manual/en/pdo.prepared-statements.php][4]
-        // get the parameters from the post. escape them to protect against sql injection.
+ parse_str(file_get_contents("php://input"),$post_vars);
 
-       $rev = mysql_real_escape_string($_GET["revenue"]);
- $whl = mysql_real_escape_string($_GET["wheels_count"]);
-$hr = mysql_real_escape_string($_GET["hours"]);
- $ic = mysql_real_escape_string($_GET["invoice_count"]);
- $mon = mysql_real_escape_string($_GET["month_id"]);
+$mod = $_POST['models'];
 
+$request = json_decode($mod);
+ 
+foreach ($request as $product) {
+ 
+$sql = "UPDATE fct_awrs_order_month
+        SET revenue='{$product->revenue}',
+        wheels_count='{$product->wheels_count}',
+        hours='{$product->hours}',
+        invoice_count='{$product->invoice_count}'
+        WHERE month_id='{$product->month_id}'
+        and franchise_id='1';";
  
 
-        $rs = mysql_query("UPDATE fct_awrs_order_month SET revenue = '" . $rev ."',wheels_count = '" . $whl ."',hours = '" . $hr ."',invoice_count = '" . $ic ."' WHERE month_id = '" .$mon. "' and franchise_id='1' ");
-if ($rs) {
-        
-        echo json_encode($rs);
-    }
-    else {
-        header("HTTP/1.1 500 Internal Server Error");
-        echo "Update failed for Month: ";
-    }
-
+$retval = mysql_query( $sql, $con );
+ 
+if(!$retval )
+{
+  echo "OUCH!";
+  //die('Could not update data: ' . mysql_error());
+}
+else
+{
+        $result3 = "Your Data is Updated Successfully";
+echo $result3;
+ 
+}
+}
+mysql_close($con);
      
 }
-*/
       
 
 ?>
