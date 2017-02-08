0bbbd84e19090cc045a0346b1f2314631e27ee64
Amit Kumar Sah <amitkumars954@gmail.com>
2017-02-08 14:46:45 +0530

    Add files via upload

--- updategoal.php
+++ updategoal.php
@@ -1,32 +1,51 @@
 <?php
 
-require 'connection.php';
+
+$con = @mysql_connect('localhost','root','demo');
+
+if(!$con)
+{
+
+	die('');
+}
 
 
-
-
- 	
  // add the header line to specify that the content type is JSON
-    header("Content-type: application/json");
+   
 
-    // determine the request type
-    $verb = $_SERVER["REQUEST_METHOD"];
+   parse_str(file_get_contents("php://input"),$post_vars);
 
+$mod = $_GET['models'];
 
-       // handle a GET 
-    if ($verb == "GET") {
-        $query2= mysql_query("select substr(a12.month_desc,1,3) month_desc,a12.month_id,a11.revenue,a11.wheels_count,a11.hours,a11.invoice_count from fct_awrs_order_month a11 left  outer join dim_month a12 on a11.month_id=a12.month_id where a11.franchise_id='1' and a12.month_end_date >'2013-01-01'
- and a12.month_start_date <'2013-12-31' group by a11.month_id,a11.franchise_id");
+$request = json_decode($mod);
+ 
+foreach ($request as $product) {
+ 
+$sql = "UPDATE fct_awrs_order_month
+        SET revenue='{$product->revenue}',
+        wheels_count='{$product->wheels_count}',
+        hours='{$product->hours}',
+        invoice_count='{$product->invoice_count}'
+        WHERE month_id='{$product->month_id}'
+        and franchise_id='1';";
+ 
+mysql_select_db('awrs');
+$retval = mysql_query( $sql, $con );
+ 
+if(!$retval )
+{
+  echo "OUCH!";
+  //die('Could not update data: ' . mysql_error());
+}
+else
+{
+        $result3 = "Your Data is Updated Successfully";
+echo $result3;
+ 
+}
+}
+mysql_close($con);
 
-$arr= array();
-
-  while ($row2=mysql_fetch_assoc($query2)) {
-
-        $arr[]=$row2;
-      }
-
-      echo json_encode($arr);
-    }
 /*
 // handle a POST  
     if ($verb == "POST") {
@@ -55,8 +74,7 @@ if ($rs) {
     }
 
      
-}
-*/
+}*/
       
 
 ?>
